64ad6ad8009f779d18d5ed5e71ee390294ed66f3
Jackson Jacobs <jacksonjjacobs@gmail.com>
2023-03-08 15:53:14 -0500

    added comments for josh

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8050;
 var conString = "postgres://efmxpdls:o-PsenD3wAW8xT3v0sIsxFQiV491eVum@ziggy.db.elephantsql.com/efmxpdls" //Can be found in the Details page
 
-
+/*HTTP REQUEST METHODS */
 const clientConnect = () => {
     var client = new pg.Client(conString);
     client.connect(function(err) {
@@ -19,18 +19,19 @@ const clientConnect = () => {
 const getUserById = (req, res) => {
     
     const id = parseInt(req.params.id)
-    client.query(`SELECT * from user where user_id = ${id}`, (err, result) => {
+    // JOSH
+    client.query(`SELECT * from user where user_id = ${id}`, (err, result) => {  
         if(err) {
             return console.error('error runn{ing query', err);
         }
-        // res.send(result.rows[0].theTime);
         console.log(`fetching id: ${id}`)
-        res.send(id)
+        res.send(result)
     });
 }
 
 const createUser = (req, res) => {
     const {id, first_name, middle_initial, last_name, email, profile_img, bio, city, state, zip, skills } = req.body
+    // JOSH
     client.query(`INSERT INTO user values( ${id}, ${first_name}, ${middle_initial}, ${last_name}, ${email}, ${profile_img}, ${bio}, ${city}, ${state}, ${zip}, ${skills})`, (err, result) => {
         if(err) {
             return console.error('error running query', err);
@@ -40,6 +41,7 @@ const createUser = (req, res) => {
     });
 }
 
+/*ENDPOINTS */
 app.get('/getId/:id', (req, res) => {
     /*Endpoint to return user profile based on user id.*/
     client = clientConnect();
@@ -48,6 +50,7 @@ app.get('/getId/:id', (req, res) => {
 })
 
 app.post('/createUser', (req, res) => {
+    /*Endpoint to create a user record based on a json object. */
     client = clientConnect();
     createUser(req, res);
     client.end();
@@ -56,3 +59,4 @@ app.post('/createUser', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
