439f8a6e8ada9d803e5058a951a73319915394b5
Joshua Tang <tang.j1905@gmail.com>
2023-03-20 22:45:37 -0400

    get course by id & field

--- server/server.js
+++ server/server.js
@@ -22,17 +22,16 @@ const port = 8050;
 const getUserById = (req, res) => {
     const id = parseInt(req.params.id)
     const query = {
-        name: 'get_user',
+        name: 'get_user_from_id',
         text: 'SELECT * FROM "user" WHERE user_id = $1',
         values: [id]
-    }
+    };
 
     client.query(query, (err, result) => {  
         if (err)
             res.status(500).send(err.message);
 
         // There should only be one row returned:
-        console.log(`Fetching id: ${id}...`)
         res.send(result.rows[0])
     });
 }
@@ -43,10 +42,13 @@ const getUserById = (req, res) => {
  * required columns (first_name, last_name, email, city, state, zip)
  * are required; unspecified columns are filled with NULL.
  * 
+ * An array of medical licenses (containing the ID and state) may also be specified;
+ * those entries will be automatically added to the table with the correct user ID.
+ * 
  * Returns the full user object, including the user's newly created ID.
  */
 const createUser = (req, res) => {
-    const column_names = [
+    const columnNames = [
         'first_name',
         'middle_initial',
         'last_name',
@@ -59,7 +61,7 @@ const createUser = (req, res) => {
         'skills'
     ];
 
-    const required_cols = [
+    const requiredCols = [
         'first_name',
         'last_name',
         'email',
@@ -69,23 +71,22 @@ const createUser = (req, res) => {
     ];
 
     const params = req.body;
-    verifyCols(params, required_cols, res);
+    verifyCols(params, requiredCols, res);
 
-    const query_values = column_names.map(col => params[col]);
+    const queryVals = columnNames.map(col => params[col]);
     const query = {
         name: 'create_user',
-        text: `INSERT INTO "user"(${column_names.join(',')})
+        text: `INSERT INTO "user"(${columnNames.join(',')})
                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
                RETURNING *`,
-        values: query_values
-    }
+        values: queryVals
+    };
 
     client.query(query, (err, result) => {
         if (err)
             res.status(500).send(err.message);
 
         // An object representing the entire user is returned!
-        console.log(`Added user with ID ${result.rows[0].user_id}...`)
         res.send(result.rows[0]);
     });
 }
@@ -98,7 +99,7 @@ const createUser = (req, res) => {
  * Returns the full course object, including the course's newly created ID.
  */
 const createCourse = (req, res) => {
-    const column_names = [
+    const columnNames = [
         'instructor_id',
         'title',
         'description',
@@ -109,23 +110,66 @@ const createCourse = (req, res) => {
     const params = req.body;
     verifyCols(params, required_cols, res);
 
-    const query_values = column_names.map(col => params[col]);
+    const queryVals = columnNames.map(col => params[col]);
     const query = {
         name: 'create_course',
-        text: `INSERT INTO course(${column_names.join(',')})
+        text: `INSERT INTO course(${columnNames.join(',')})
                VALUES ($1, $2, $3, $4, $5)
                RETURNING *`,
-        values: query_values
+        values: queryVals
     };
 
     client.query(query, (err, result) => {
         if (err)
             res.status(500).send(err.message);
-
-        console.log(`Added course with ID ${result.rows[0].course_id}...`);
         res.send(result.rows[0]);
     });
 }
+
+/**
+ * Retrieves a course by its ID.
+ */
+const getCourseById = (req, res) => {
+    const id = parseInt(req.params.id)
+    const query = {
+        name: 'get_course_from_id',
+        text: 'SELECT * FROM course WHERE course_id = $1',
+        values: [id]
+    };
+
+    client.query(query, (err, result) => {  
+        if (err)
+            res.status(500).send(err.message);
+        res.send(result.rows[0]);
+    });
+}
+
+/**
+ * Retrieves one or more course entries by field,
+ * returning the result as an array of entries.
+ * 
+ * This performs a substring match, fetching all courses
+ * that contain the queried field.
+ */
+const getCoursesByField = (req, res) => {
+    const field = decodeURI(req.params.field);
+    const query = {
+        name: 'get_course_from_field',
+        text: 'SELECT * FROM course WHERE POSITION($1 IN field) > 0',
+        values: [field]
+    };
+
+    client.query(query, (err, result) => {
+        if (err)
+            res.status(500).send(err.message);
+        res.send(result.rows);
+    });
+}
+
+/**
+ * Retrieves one or more courses by location,
+ * returning the result as an array of entries.
+ */
 
 /* --------------------------------- */
 /*     MIDDLEWARE/INPUT HANDLING     */
@@ -147,7 +191,7 @@ const verifyCols = (body, required_cols, res) => {
 /* --------------------------------- */
 /*           HTTP ENDPOINTS          */
 /* --------------------------------- */
-app.get('/getId/:id', (req, res) => {
+app.get('/getUser/id/:id', (req, res) => {
     /* Endpoint to return user profile based on user id. */
     getUserById(req, res);
 })
@@ -155,6 +199,16 @@ app.get('/getId/:id', (req, res) => {
 app.post('/createUser', (req, res) => {
     /* Endpoint to create a user record based on a json object. */
     createUser(req, res);
+})
+
+app.get('/getCourse/id/:id', (req, res) => {
+    /* Endpoint to return course information based on course id. */
+    getCourseById(req, res);
+})
+
+app.get('/getCourse/field/:field', (req, res) => {
+    /* Endpoint to return courses matching a particular field. */
+    getCoursesByField(req, res);
 })
 
 app.post('/createCourse', (req, res) => {
